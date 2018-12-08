febf80fff1ff539344c1675cc21230a5ea11517e
notjon <notjon@notjons-imac.local>
2018-12-08 13:29:56 -0700

    Initial Commit

---
+++ idevicererestore/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  idevicererestore
+//
+//  Created by notjon on 12/8/18.
+//  Copyright © 2018 a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
